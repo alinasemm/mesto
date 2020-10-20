3676bf474c7467dd965ebde75a6c10b9240850a1
Alina Semikolenova <alinasemmm@yandex.ru>
2020-10-20 22:02:02 +0300

    Обработала событие submit для формы

--- pages/script.js
+++ pages/script.js
@@ -1,32 +1,29 @@
 let editButton = document.querySelector("#edit-button");
-let popup = document.querySelector(".popup")
-editButton.addEventListener("click", function(){
-    popup.classList.add("popup_opened")
+let popup = document.querySelector(".popup");
+editButton.addEventListener("click", function () {
+    popup.classList.add("popup_opened");
 })
 
 let closeIcon = document.querySelector("#close-icon");
-closeIcon.addEventListener("click", function(){
-    popup.classList.remove("popup_opened")
-})
+function closePopup () {
+    popup.classList.remove("popup_opened");
+}
+
+closeIcon.addEventListener("click", closePopup);
 
 let formElement = document.querySelector("#container");
+let nameInput = document.querySelector("#name");
+let jobInput = document.querySelector("#job");
+let nameProfile = document.querySelector("#profile-name");
+let jobProfile = document.querySelector("#profile-job");
+
 function formSubmitHandler (event) {
     event.preventDefault();
-    let nameInput = document.querySelector("#name");
-    let jobInput = document.querySelector("#job");
-
-    let name = nameInput.value;
-    let job = jobInput.value;
-
-    let nameProfile = document.querySelector("#profile-name");
-    let jobProfile = document.querySelector("#profile-job");
-
+    
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
     
+    closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler); 
-
-
-
