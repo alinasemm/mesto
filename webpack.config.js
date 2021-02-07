9ba540e7f7208644d0a45950080315d6007fc571
Alina Semikolenova <alinasemmm@yandex.ru>
2021-02-07 23:24:44 +0300

    Продолжение настройки вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,54 @@
 const path = require('path');
 // import path from 'path'
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
 
 module.exports = {
   entry: './scripts/script.js', // точка входа - главный js файл
   output: { // точка выхода - куда положить новый, преобразованный файл (он же бандл - bundle)
     path: path.resolve(__dirname, 'dist'), // destination - назначение пути
+    publicPath: '',
     filename: 'bundle.js'
   },
   devServer: {
     contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
     compress: true, // это ускорит загрузку в режиме разработки
     open: true // сайт будет открываться сам при запуске npm run dev
-  }
+  },
+  module: {
+    rules: [ // rules — это массив правил
+      // добавим в него объект правил для бабеля
+      {
+        // регулярное выражение, которое ищет все js файлы
+        test: /\.js$/,
+        // при обработке этих файлов нужно использовать babel-loader
+        use: 'babel-loader',
+        // исключает папку node_modules, файлы в ней обрабатывать не нужно
+        exclude: '/node_modules/'
+      },
+      {
+        // регулярное выражение, которое ищет все файлы с такими расширениями
+        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource'
+      },
+      {
+        // применять это правило только к CSS-файлам
+        test: /\.css$/,
+        // при обработке этих файлов нужно использовать
+        // MiniCssExtractPlugin.loader и css-loader
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader', 
+          options: { importLoaders: 1 }
+        }, 'postcss-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' // путь к файлу index.html
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
+  ]  
 };
