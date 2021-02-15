5f7e2442c8384fef4b3432893a030e8b8363a128
Alina Semikolenova <alinasemmm@yandex.ru>
2021-02-15 16:45:26 +0300

    Сделала загрузку карточек с сервера

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,30 @@
+import { token, groupId } from "../config";
 export default class Section {
-  constructor({ items, renderer }, containerSelector) {
-    this._renderedItems = items;
+  constructor({ renderer }, containerSelector) {
     this._renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
+  _fetchCards() {
+    return fetch(`https://mesto.nomoreparties.co/v1/${groupId}/cards`, {
+      headers: {
+        authorization: token
+      }
+    })  
+    .then(res => {
+      return res.json();
+    });
+  }
+
   // Отрисовка элементов
   renderItems() {
-    this._renderedItems.forEach(item => this._renderer(item));
+    this._fetchCards()
+      .then(initialItems => {
+        initialItems.forEach(item => this._renderer(item));
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   // Добавить элемент 
